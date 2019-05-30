8023dec98f6f2e0513b5f764f14aea23b440ddd4
goodapple8946 <goodapple8946@163.com>
2019-05-31 00:14:53 +0800

    update search limits

--- MT_WEB/src/main/java/com/woxue/mt/Program/Program.java
+++ MT_WEB/src/main/java/com/woxue/mt/Program/Program.java
@@ -1,13 +1,7 @@
 package com.woxue.mt.Program;
 
-import com.woxue.mt.sqldealer.Professor;
-import com.woxue.mt.sqldealer.SqlDealer;
-import com.woxue.mt.sqldealer.Thesis;
-import com.woxue.mt.sqldealer.User;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.woxue.mt.sqldealer.*;
+import java.util.*;
 
 public class Program
 {
@@ -17,60 +11,19 @@ public class Program
         {
             SqlDealer sqlDealer = new SqlDealer();
 
-            List<String> keywords = new ArrayList<String>(Arrays.asList("slow", "OCTAHEDRAL"));
-
-            //根据关键词搜索论文
+            //高级搜索论文（标旗、作者、关键词、年份），显示区间 [limitStart, limitEnd)
             {
-                List<Thesis> theses;
-                theses = sqlDealer.searchThesisAnd(keywords);  //与
-                theses = sqlDealer.searchThesisOr(keywords);   //或
+                List<String> titles = new ArrayList<>(Arrays.asList("slow"));
+                List<String> authors = new ArrayList<>(Arrays.asList("Lei", "Chen"));
+                List<String> keywords = null;   //为null时，代表匹配内容任意
+                String yearStart = "2010";
+                String yearEnd = "2020";
+                Integer limitStart = 0; //从0开始
+                Integer limitEnd = 1;   //此时检索区间为[0,1)
+
+                List<Thesis> theses = sqlDealer.advancedSearchThesisAnd(titles, authors, keywords, yearStart, yearEnd, limitStart, limitEnd);
                 for (Thesis thesis : theses)
                     thesis.display();
-            }
-
-            //更新科技专家（根据科技专家ID）
-            {
-                Professor professor = new Professor();
-                professor.id = "test";
-                sqlDealer.updateProfessor(professor);
-            }
-
-            //科技专家（limit：显示数量）
-            {
-                List<Professor> professors = sqlDealer.searchProfessor(1);
-                for (Professor professor : professors)
-                    professor.display();
-            }
-
-            //所有科技专家数量
-            {
-                int countProfessor = sqlDealer.countProfessor();
-                System.out.println("--科技专家数量--\n" + countProfessor);
-            }
-
-            //插入用户
-            {
-                User user = new User();
-                user.id = "test";
-                user.nickname = "A";
-                user.password = "123";
-                sqlDealer.insertUser(user);
-            }
-
-            //更新用户（根据用户ID）
-            {
-                User user = new User();
-                user.id = "test";
-                user.nickname = "B";
-                user.password = "456";
-                sqlDealer.updateUser(user);
-            }
-
-            //所有用户
-            {
-                List<User> users = sqlDealer.searchUser();
-                for (User user : users)
-                    user.display();
             }
         }
         catch (Exception e)
--- MT_WEB/src/main/java/com/woxue/mt/sqldealer/SqlDealer.java
+++ MT_WEB/src/main/java/com/woxue/mt/sqldealer/SqlDealer.java
@@ -6,12 +6,16 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class SqlDealer
 {
     private SqlSession sqlSession;
 
+    //构造函数
     public SqlDealer() throws Exception
     {
         String resource = "mybatis.xml";
@@ -21,91 +25,226 @@ public class SqlDealer
         sqlSession = sqlSessionFactory.openSession();
     }
 
-    public List<Thesis> searchThesis(int limit)
+    /**
+     * 搜索未审核论文
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束索引（不包括）
+     * @return 搜索结果
+     */
+    public List<Thesis> searchThesisUnverified(int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchThesis", limit);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchThesisUnverified", args);
     }
 
-    public List<Thesis> searchThesisUnverified(int limit)
+    /**
+     * 搜索论文，与关系
+     * @param keywords 关键词，null代表任意
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束索引（不包括）
+     * @return 搜索结果
+     */
+    public List<Thesis> searchThesisAnd(List<String> keywords, int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchThesisUnverified", limit);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("keywords", keywords != null ? keywords : new ArrayList<>(Arrays.asList(".")));
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchThesisAnd", args);
     }
 
-    public List<Thesis> searchThesisAnd(List<String> keywords)
+    /**
+     * 搜索论文，或关系
+     * @param keywords 关键词，null代表任意
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束索引（不包括）
+     * @return 搜索结果
+     */
+    public List<Thesis> searchThesisOr(List<String> keywords, int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchThesisAnd", keywords);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("keywords", keywords != null ? keywords : new ArrayList<>(Arrays.asList(".")));
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchThesisOr", args);
     }
 
-    public List<Thesis> searchThesisOr(List<String> keywords)
+    /**
+     * 高级搜索论文，与关系
+     * @param titles 主题，null代表任意
+     * @param authors 作者，null代表任意
+     * @param keywords 关键词，null代表任意
+     * @param yearStart 起始年份（包括），范围(0000~9999)
+     * @param yearEnd 结束年份（不包括），范围(0000~9999)
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public List<Thesis> advancedSearchThesisAnd(List<String> titles, List<String> authors, List<String> keywords, String yearStart, String yearEnd, int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchThesisOr", keywords);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("titles", titles != null ? titles : new ArrayList<>(Arrays.asList(".")));
+        args.put("authors", authors != null ? authors : new ArrayList<>(Arrays.asList(".")));
+        args.put("keywords", keywords != null ? keywords : new ArrayList<>(Arrays.asList(".")));
+        args.put("yearStart", yearStart);
+        args.put("yearEnd", yearStart);
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.advancedSearchThesisAnd", args);
     }
 
+    /**
+     * 更新论文
+     * @param thesis 更新后的论文，以ID为目标
+     */
     public void updateThesis(Thesis thesis)
     {
         sqlSession.update("mapper.updateThesis", thesis);
         sqlSession.commit();
     }
 
-    public List<Professor> searchProfessor(int limit)
+    /**
+     * 搜索科技专家
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public List<Professor> searchProfessor(int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchProfessor", limit);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchProfessor", args);
     }
 
-    public List<Professor> searchProfessorUnverified(int limit)
+    /**
+     * 搜索未审核科技专家
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public List<Professor> searchProfessorUnverified(int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchProfessorUnverified", limit);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchProfessorUnverified", args);
     }
 
-    public List<Professor> searchProfessorByArea(String area)
+    /**
+     * 根据领域搜索专家
+     * @param area 领域
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public List<Professor> searchProfessorByArea(String area, int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchProfessor", area);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        args.put("area", area);
+        return sqlSession.selectList("mapper.searchProfessor", args);
     }
 
+    /**
+     * 专家数量
+     * @return 专家数量
+     */
     public int countProfessor()
     {
         return sqlSession.selectOne("mapper.countProfessor");
     }
 
+    /**
+     * 更新专家
+     * @param professor 更新后的专家，以ID为目标
+     */
     public void updateProfessor(Professor professor)
     {
         sqlSession.update("mapper.updateProfessor", professor);
         sqlSession.commit();
     }
 
-    public List<User> searchUser()
+    /**
+     * 搜索用户
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public List<User> searchUser(int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchUser");
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchUser", args);
     }
 
-    public User searchUserById(String Id)
+    /**
+     * 根据ID搜索用户
+     * @param Id 用户ID
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public User searchUserById(String Id, int limitStart, int limitEnd)
     {
-        return sqlSession.selectOne("mapper.searchUser", Id);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("Id", Id);
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectOne("mapper.searchUser", args);
     }
 
+    /**
+     * 更新用户
+     * @param user 更新后的用户，以ID为目标
+     */
     public void updateUser(User user)
     {
         sqlSession.update("mapper.updateUser", user);
         sqlSession.commit();
     }
 
+    /**
+     * 插入用户
+     * @param user 插入的用户
+     */
     public void insertUser(User user)
     {
         sqlSession.insert("mapper.insertUser", user);
         sqlSession.commit();
     }
 
+    /**
+     * 根据ID删除用户
+     * @param Id 删除的ID
+     */
     public void deleteUserById(String Id)
     {
         sqlSession.delete("mapper.deleteUserById", Id);
         sqlSession.commit();
     }
 
-    public List<Trend> searchTrendByProfessorId(String Id)
+    /**
+     * 根据ID搜索动态
+     * @param Id 动态ID
+     * @param limitStart 起始索引（包括）
+     * @param limitEnd 结束年份（不包括）
+     * @return 搜索结果
+     */
+    public List<Trend> searchTrendByProfessorId(String Id, int limitStart, int limitEnd)
     {
-        return sqlSession.selectList("mapper.searchTrendByProfessorId", Id);
+        HashMap<String, Object> args = new HashMap<>();
+        args.put("Id", Id);
+        args.put("limit1", limitStart);
+        args.put("limit2", limitEnd - limitStart);
+        return sqlSession.selectList("mapper.searchTrendByProfessorId", args);
     }
 
+    //析构函数
     protected void finalize()
     {
         sqlSession.close();
